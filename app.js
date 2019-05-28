81aa6f489db5fe290efa7d40575a58cd451d71e6
Olawumi Yusuff <qauzeemyusuff2@gmail.com>
2019-05-28 19:55:22 +0100

    installed nodemon and morgan

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const app = express();
+const morgan = require('morgan');
 const productRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
+
+const app = express();
+app.use(morgan('dev'));
 
 
 app.use('/products', productRoutes);
