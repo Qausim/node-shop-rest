a239d73269c1f391482ebe13e24915839fd2858d
Olawumi Yusuff <qauzeemyusuff2@gmail.com>
2019-05-30 17:34:16 +0100

    cleared unneeded GET /users route

---
+++ api/routes/user.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+
+const router = express.Router();
+
+
+router.post('/signup', (request, response, next) => {
+    User.find({email: request.body.email})
+        .then(existingUser => {
+            if (existingUser.length) {
+                const existError = new Error();
+                existError.status = 409;
+                existError.message = 'Mail exists';
+                throw existError;
+            } else {
+                bcrypt.hash(request.body.password, 10, (error, hash) => {
+                    if (error) {
+                        error.status = 500;
+                        throw error;
+                    } else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: request.body.email,
+                            password: hash
+                        });
+                        return user.save()
+                            .then(user => {
+                                console.log(user);
+                                response.status(201).json({
+                                    message: 'User created'
+                                })
+                            })
+                            .catch(err => {
+                                response.status(500).json({
+                                    error: err
+                                });
+                            });
+                    }
+                });
+            }
+        })
+        .catch(error => response.status(error.status || 500).json({
+            error
+        }));
+});
+
+router.post('/signin', (request, response, next) => {
+
+});
+
+router.delete('/:id', (request, response, next) => {
+    const id = request.params.id;
+    User.findOneAndRemove({_id: id})
+        .then(user => {
+            if (user) {
+                response.status(200).json({
+                    message: 'Deleted',
+                    user
+                });
+            } else {
+                const error = new Error();
+                error.message = 'Not found';
+                throw error;
+            }
+        })
+        .catch(error => response.status(404).json({ error }));
+});
+
+
+
+module.exports = router;
